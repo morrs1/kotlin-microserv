812a67d41ce7c9916526d862fa32fae24e90674f
morrs <grishkovea@gmail.com>
2025-04-24 00:40:49 +0300

    feat: добавил метод в сервис для создания заказа

--- kotlin-micro-order/src/main/kotlin/org/example/kotlinmicroorder/services/OrderService.kt
+++ kotlin-micro-order/src/main/kotlin/org/example/kotlinmicroorder/services/OrderService.kt
@@ -2,10 +2,13 @@ package org.example.kotlinmicroorder.services
 
 import org.example.kotlinmicroorder.dao.OrderDao
 import org.example.kotlinmicroorder.dto.UsersOrder
+import org.example.kotlinmicroorder.dto.UsersOrderRequest
+import org.example.kotlinmicroorder.dto.UsersOrderResponse
 import org.example.kotlinmicroorder.models.Order
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDate
 import java.util.*
 
 @Service
@@ -20,6 +23,22 @@ class OrderService {
     fun create(order: Order) {
         order.id = UUID.randomUUID()
         orderDao.create(order)
+    }
+
+    @Transactional
+    fun create(order: UsersOrderRequest): UsersOrderResponse {
+        val newOrder = UsersOrderResponse(
+            order.usersId,
+            order.usersEmail,
+            UUID.randomUUID(),
+            LocalDate.now(),
+            "Создан",
+            order.listOfProducts
+        )
+
+        //TODO добавление в бд
+
+        return newOrder
     }
 
     fun getUsersOrder(orderId: UUID): UsersOrder? {
